0c78a2171c4c4a38649bf485f809219dd14be9c4
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2020-12-04 14:22:48 +0200

    done, all test are passed

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,15 @@
 package core.basesyntax;
 
-public class UserService {
+import core.basesyntax.exception.UserNotFoundException;
 
+public class UserService {
     public int getUserScore(String[] records, String email) {
-        return 0;
+        for (String user : records) {
+            String[] splittedEmails = user.split(":");
+            if (splittedEmails[0].equals(email)) {
+                return Integer.parseInt(splittedEmails[1]);
+            }
+        }
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
